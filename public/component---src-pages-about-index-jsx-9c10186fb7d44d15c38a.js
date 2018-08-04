f7042ef066634eb3194bdabc733b855ca5f9ab37
Michael Kim <micklepickle98@gmail.com>
2018-08-03 21:17:53 -0400

    Add public

---
+++ public/component---src-pages-about-index-jsx-9c10186fb7d44d15c38a.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x69ade38bc1ad],{206:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var a=l(2),n=u(a),d=function(){return n.default.createElement("div",{className:"box"},n.default.createElement("p",null,"Hello"))};t.default=d,e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-about-index-jsx-9c10186fb7d44d15c38a.js.map
---
+++ public/path----680f2147d59dcbca83ee.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Michael Kim"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----680f2147d59dcbca83ee.js.map
